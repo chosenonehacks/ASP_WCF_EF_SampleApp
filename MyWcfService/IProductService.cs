732f3e338b8f0244a52e58d0b2bb28a2e4e3187b
xorch <hal9k2@outlook.com>
2016-01-23 15:36:29 +0100

    Initial commit

---
+++ MyWcfService/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProductService" in both code and config file together.
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        List<Product> GetAllProducts();
+
+        [OperationContract]
+        List<Product> GetProductsById(int id);
+
+        [OperationContract]
+        List<Product> GetProductsByDate(DateTime creationDate);
+    }
+}
---
+++ MyWcfService/ProductService.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductService.svc or ProductService.svc.cs at the Solution Explorer and start debugging.
+    public class ProductService : IProductService
+    {
+        private SampleDbEntities _dbEntities = new SampleDbEntities();
+
+        public List<Product> GetAllProducts()
+        {
+            return _dbEntities.Products.ToList();
+        }
+
+        public List<Product> GetProductsByDate(DateTime creationDate)
+        {
+            return _dbEntities.Products.Where(p => p.CreationDate == creationDate).ToList();
+        }
+
+        public List<Product> GetProductsById(int id)
+        {
+            return _dbEntities.Products.Where(p => p.Id == id).ToList();
+        }
+    }
+}
